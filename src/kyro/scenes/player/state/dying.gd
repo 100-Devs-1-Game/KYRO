e54b43105741eb8811e5ac94c82090235360e36c
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-15 04:43:16 -0400

    Add dying animation

--- src/kyro/scenes/player/state/dying.gd
+++ src/kyro/scenes/player/state/dying.gd
@@ -5,7 +5,7 @@ func _state_entered() -> void:
 	owner.hud.visible = false
 	owner.game_over.visible = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	Engine.time_scale = 0.8
+	owner.crouch_animation_player.play(&"dying")
 
 
 func _state_process(delta: float) -> void:
