046ef01c3dc77205ef0060c4308d80349ec44ceb
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-16 08:43:39 -0400

    Fix ammo pickups

--- src/kyro/scenes/pickup/ammo/ammo_pickup.gd
+++ src/kyro/scenes/pickup/ammo/ammo_pickup.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 
 func _on_pickup_area_picked_up(by: Node) -> void:
+	if not is_instance_valid(by.gun_manager):
+		return
+	
 	by.gun_manager.reserve_ammo += 1
 	by.gun_manager.ammo_counts_updated.emit()
 	queue_free()
