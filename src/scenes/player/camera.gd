79121966e8b43bd4914c0b722c091dee44c4354c
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-25 15:05:54 -0400

    Add camera FOV effect

--- src/scenes/player/camera.gd
+++ src/scenes/player/camera.gd
@@ -7,7 +7,18 @@ extends Camera3D
 @export var shake_trauma_max:float = 2
 @export var shake_rotation_max:Vector3 = Vector3(PI, PI, 0.0)
 @export var shake_offset_max:Vector2 = Vector2(0.05, 0.05)
+@export_group("Fov effect", "fov_")
+## The maximum that the effect approaches
+@export_range(0.0, 179.0, 0.1, "suffix:°") var fov_effect_increase:float = 25.0
+## How fast the effect ramps to maximum
+@export var fov_effect_exp:float = 1.1
+@export var fov_effect_start:float = 15
+@export var fov_effect_speed:float = 17
 
+
+
+var base_fov:float
+var target_fov:float
 
 var time:float
 var shake_trauma:float = 0:
@@ -16,11 +27,27 @@ var shake_trauma:float = 0:
 var shake_rotation:Vector3
 
 
+func _ready() -> void:
+	base_fov = fov
+
+
 func _process(delta: float) -> void:
 	if not is_zero_approx(shake_trauma):
 		shake()
 		time += delta
 		shake_trauma /= 1 + shake_trauma_decay * delta
+	_recalculate_fov_effect()
+	print((owner.velocity).dot(-owner.global_basis.z))
+	fov = lerpf(fov, target_fov, delta * fov_effect_speed)
+
+
+func _recalculate_fov_effect() -> void:
+	var forward_speed_normalized:float = \
+			(owner.velocity).dot(-owner.global_basis.z)
+	forward_speed_normalized = minf(-forward_speed_normalized + fov_effect_start, 0.0)
+	
+	target_fov = base_fov + (-fov_effect_exp ** (forward_speed_normalized)) \
+			* fov_effect_increase + fov_effect_increase
 
 
 func shake():
