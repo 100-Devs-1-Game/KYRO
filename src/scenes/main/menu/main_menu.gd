551d8af194c69e9101886a0981aa5f7f5ff16418
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-26 11:59:37 -0400

    Themin'

--- src/scenes/main/menu/main_menu.gd
+++ src/scenes/main/menu/main_menu.gd
@@ -4,18 +4,40 @@ extends CanvasLayer
 signal play_level_requested(level_path:String)
 
 
+enum MenuMode {
+	BUTTONS,
+	LEVEL_SELECT,
+	OPTIONS,
+}
+
+
 const LEVEL_BUTTON_SCENE:PackedScene = preload("res://scenes/main/menu/level_button.tscn")
 
 
 @export var levels:Array[LevelData] = []
 
 
+var menu_mode:MenuMode = MenuMode.BUTTONS:
+	set(new):
+		menu_mode = new
+		for i in len(menus):
+			menus[i].visible = i == menu_mode
+
+
 @onready var menu_buttons:Control = %Buttons
 @onready var level_buttons:Control = %Levels
+@onready var menus:Array[Control] = [menu_buttons, level_buttons]
 
 
 func _ready() -> void:
 	make_level_buttons()
+	menu_mode = menu_mode
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action(&"ui_cancel"):
+		if menu_mode != MenuMode.BUTTONS:
+			menu_mode = MenuMode.BUTTONS
 
 
 func make_level_buttons() -> void:
@@ -27,13 +49,10 @@ func make_level_buttons() -> void:
 
 
 func reset() -> void:
-	menu_buttons.visible = true
-	level_buttons.visible = false
-
+	menu_mode = MenuMode.BUTTONS
 
 func _on_select_level_pressed() -> void:
-	menu_buttons.visible = false
-	level_buttons.visible = true
+	menu_mode = MenuMode.LEVEL_SELECT
 
 
 func _on_quit_pressed() -> void:
