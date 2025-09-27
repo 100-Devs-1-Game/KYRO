c4d1b16417976d1d52c501fc8622d75b29276e22
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-27 15:00:35 -0400

    Options menu framework

---
+++ src/globals/autoloads/options/menu/audio_sliders.gd
@@ -0,0 +1,27 @@
+extends GridContainer
+
+const AUDIO_SLIDER:PackedScene = preload("uid://dt02v7n4ecwhq")
+
+@export var sliders:PackedStringArray 
+
+
+func _ready() -> void:
+	for bus in AudioServer.bus_count:
+		var bus_name:String = AudioServer.get_bus_name(bus)
+		make_audio_slider(bus_name, bus)
+
+
+func make_audio_slider(bus_name:String, bus_index:int) -> void:
+	var label := Label.new()
+	label.name = bus_name
+	label.text = bus_name
+	add_child(label)
+	var audio_slider:HSlider = AUDIO_SLIDER.instantiate()
+	audio_slider.value = AudioServer.get_bus_volume_linear(bus_index)
+	audio_slider.name = bus_name + "Slider"
+	audio_slider.value_changed.connect(_update_bus_volume.bind(bus_index))
+	add_child(audio_slider)
+
+
+func _update_bus_volume(value:float, bus_index:int) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
