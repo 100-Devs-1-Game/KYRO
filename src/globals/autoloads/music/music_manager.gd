2ba0d8bd913d2e0b2471be99260287fc4dd23fcd
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-27 02:07:33 -0400

    NOW PLAYING
    Now playing - Your song

--- src/globals/autoloads/music/music_manager.gd
+++ src/globals/autoloads/music/music_manager.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 @onready var music_player:AudioStreamPlayer = $MusicPlayer
+@onready var animation_player:AnimationPlayer = $AnimationPlayer
+@onready var track_name:Label = %TrackName
 
 
 func song_request(song: StringName) -> void:
@@ -13,5 +15,7 @@ func song_request(song: StringName) -> void:
 	var playback: AudioStreamPlaybackInteractive = music_player.get_stream_playback()
 	if song == music_player.stream.get_clip_name(playback.get_current_clip_index()):
 		return
-
+	
+	track_name.text = song
+	animation_player.play(&"track_display")
 	playback.switch_to_clip_by_name(song)
