c00e0ec5f954f710ce6ac96eebb0c087c39d9db7
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-09-27 12:01:52 -0400

    Wallride coyote time

--- src/scenes/player/state/wallride.gd
+++ src/scenes/player/state/wallride.gd
@@ -2,21 +2,27 @@ extends State
 
 const WALLRIDE_CAMERA_ROLL:float = PI/54
 
+
 @export var forward_speed_mod:float = 1.2
 @export var gravity_mod:float = 0.2
 @export var mod_decay_curve:Curve # TODO: this should probably be baked
 @export var jump_impulse:float = 30
 @export var jump_forward_impulse:float = 8
+@export var wallride_coyote_time_max:float = 0.30
 
 @export_group("State Connectons", "state_")
 @export var state_jump:State
+@export var state_walk:State
 
 
 var wallride_time:float = 0.0
+var wallride_coyote_time:float = 0.0
+var wallride_axis:float
 
 
 func _state_entered() -> void:
 	wallride_time = 0
+	wallride_coyote_time = 0
 	owner.velocity = owner.state_commons.dampen_vector_axis(owner.velocity, owner.global_basis.y, 1.5)
 
 
@@ -25,11 +31,14 @@ func _state_process(delta: float) -> void:
 
 
 func _state_physics_process(delta: float) -> void:
+	if owner.state_commons.can_wallride():
+		wallride_axis = owner.wallride_axis
+	
 	if state_jump.jump_wanted():
 		owner.velocity = owner.state_commons.set_vector_axis(
 			owner.velocity, 
 			owner.global_basis.x, 
-			jump_impulse * roundf(owner.wallride_axis)
+			jump_impulse * roundf(wallride_axis)
 		)
 		owner.velocity = owner.state_commons.set_vector_axis(
 			owner.velocity,
@@ -52,9 +61,15 @@ func _state_physics_process(delta: float) -> void:
 	owner.state_commons.do_post_slide_updates(delta)
 	
 	wallride_time += delta
+	
+	
+	if owner.is_on_floor():
+		machine.to_state(state_walk)
+	
 	if not owner.state_commons.can_wallride():
-		var modal_basic:State = owner.state_commons.get_modal_basic_state()
-		machine.to_state(modal_basic)
+		wallride_coyote_time += delta
+		if wallride_coyote_time > wallride_coyote_time_max:
+			machine.to_state(owner.state_commons.get_modal_basic_state())
 
 
 func _state_exited() -> void:
